7d924260fdce762b305e1acbee85920a3661a432
dmnlk <seikima2demon@gmail.com>
2014-12-27 23:00:45 +0900

    add example and, stringUtils

---
+++ example/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dmnlk/stringUtils"
+)
+
+
+func main() {
+	a := "test"
+	fmt.Println(stringUtils.IsEmpty(a))
+}
---
+++ stringUtils.go
@@ -0,0 +1,9 @@
+package  stringUtils
+
+import (
+
+)
+
+func IsEmpty(s string) bool {
+	return true
+}
