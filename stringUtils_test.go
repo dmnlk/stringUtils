0e19a1a4c2d7a555e037fc0bc6eb22d0a2a394d2
dmnlk <seikima2demon@gmail.com>
2014-12-27 23:16:09 +0900

    add test code

--- stringUtils_test.go
+++ stringUtils_test.go
@@ -6,6 +6,11 @@ func TestIsEmpty(t *testing.T) {
 	blank := ""
 	actual := IsEmpty(blank)
 	if actual == false {
-		t.Fail()
+		t.Errorf("fail test, black should empty")
+	}
+	nilString := nil
+	actual = IsEmpty(nilString)
+	if actual == false {
+		t.Error("fail test, nil should empty")
 	}
 }
