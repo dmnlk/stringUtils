7c0284f6b26916f5b8af4f093bdc487b37c482dd
dmnlk <seikima2demon@gmail.com>
2014-12-28 22:19:57 +0900

    write godoc

--- stringUtils.go
+++ stringUtils.go
@@ -1,5 +1,7 @@
+//port ApacheCommons lang string utils
 package stringUtils
 
+// Check if string is blank
 func IsEmpty(s string) bool {
 	if len(s) == 0 {
 		return true
@@ -7,10 +9,12 @@ func IsEmpty(s string) bool {
 	return false
 }
 
+// Check if string is not blank
 func IsNotEmpty(s string) bool {
 	return !IsEmpty(s)
 }
 
+// Check if any one of strings are blank
 func IsAnyEmpty(strings ...string) bool {
 	for _, v := range strings {
 		if len(v) == 0 {
